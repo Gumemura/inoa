7b3935a6a12decb1f86043fc986a350f66e0ea90
gumemura <gumemura123@gmail.com>
2020-12-09 03:48:22 +0000

    setup da pag inicial

--- flask_app.py
+++ flask_app.py
@@ -4,9 +4,7 @@ import yfinance as yf
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments = []
-
-msft = yf.Ticker("MSFT")
+'''msft = yf.Ticker("MSFT")
 
 tickers = ['MGLU3']
 
@@ -14,18 +12,11 @@ for ticker in tickers:
     ticker_yahoo = yf.Ticker(ticker+'.SA')
     data = ticker_yahoo.history()
     last_quote = (data.tail(1)['Close'].iloc[0])
-    print(ticker, last_quote)
+    print(ticker, last_quote)'''
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+    return render_template("main_interface.html")
+print('asd')
 
-    comments.append(request.form["contents"])
-    print(comments)
-    return redirect(url_for('index'))
-
-@app.route('/vmd_timestamp')
-def test():
-    return render_template('main_interface.html')
 
