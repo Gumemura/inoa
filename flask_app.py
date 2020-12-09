ebe86c248bc75c59b73ed3b237bf7d25416ba19e
gumemura <gumemura123@gmail.com>
2020-12-09 03:27:35 +0000

    pagina principal

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,20 @@
 from flask import Flask, redirect, render_template, request, url_for
+import yfinance as yf
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 comments = []
+
+msft = yf.Ticker("MSFT")
+
+tickers = ['MGLU3']
+
+for ticker in tickers:
+    ticker_yahoo = yf.Ticker(ticker+'.SA')
+    data = ticker_yahoo.history()
+    last_quote = (data.tail(1)['Close'].iloc[0])
+    print(ticker, last_quote)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -13,3 +24,8 @@ def index():
     comments.append(request.form["contents"])
     print(comments)
     return redirect(url_for('index'))
+
+@app.route('/vmd_timestamp')
+def test():
+    return render_template('main_interface.html')
+
