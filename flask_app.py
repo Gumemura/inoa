c087ed8b0061681e7a3dc9986b7e8cdcb1f77cfe
gumemura <gumemura123@gmail.com>
2020-12-21 02:27:46 +0000

    feito modelo de acoes a serem armazenadas | feita pag de registro de acoes

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, render_template, request, url_for, flash
+from flask import Flask, redirect, render_template, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-import yfinance as yf
+#import yfinance as yf
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,7 +10,7 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
     username = "gumemura",
     password = "leonscot",
     hostname = "gumemura.mysql.pythonanywhere-services.com",
-    databasename = "gumemura$users",
+    databasename = "gumemura$Inoa",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
@@ -32,32 +32,89 @@ class Users(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    userEmail = db.Column(db.String(4096))
+    email = db.Column(db.String(4096))
+    shares = db.relationship('SharesTrack', backref = 'user', lazy=True)
+    alert = db.relationship('EmailShooter', backref = 'user', lazy=True)
+
+class SharesTrack(db.Model):
+    __tablename__ = "storedShares"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(4096))
+    dateFrom = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class EmailShooter(db.Model):
+    __tablename__ = "emailAlert"
+
+    id = db.Column(db.Integer, primary_key=True)
+    share = db.Column(db.String(4096))
+    higher = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("main_interface.html", users = Users.query.all())
 
-    email = request.form["userEmail"]
+    session['userEmail'] = request.form["userEmail"]
+
     emailValidator = 0
-    for i in range(len(email)):
-        if email[i] == '@' and i != 0:
+    for i in range(len(session['userEmail'])):
+        if session['userEmail'][i] == '@' and i != 0:
             emailValidator = emailValidator + 1
             aIndex = i
-        if emailValidator == 1 and  aIndex + 1 < i < len(email) - 1:
-            if email[i] == '.':
+        if emailValidator == 1 and  aIndex + 1 < i < len(session['userEmail']) - 1:
+            if session['userEmail'][i] == '.':
                 emailValidator = emailValidator + 1
                 break
 
     if emailValidator == 2:
-        user = Users(userEmail = request.form["userEmail"])
-        db.session.add(user)
-        db.session.commit()
+        newUser = True
+        users = Users.query.all()
+
+        for user in users:
+            if user.email == session['userEmail']:
+                newUser = False
+                break
+
+        if newUser:
+            user = Users(email = session['userEmail'])
+            db.session.add(user)
+            db.session.commit()
+
+        return redirect(url_for('registerShares'))
     else:
-        flash("Insert valid e-mail!")
+        return render_template("main_interface.html", error=True)
 
     return redirect(url_for('index'))
 
+@app.route("/register_shares/", methods=["GET", "POST"])
+def registerShares():
+    if request.method == "GET":
+        return render_template("register_shares.html", currentUser = session['userEmail'])
+
+    #currentUserId = Users.query.filter_by(email = currentUser).first().id
+    name = request.form["newShare"],
+    dateFrom = request.form["fromDate"]
+
+    if name != '':
+        newShare = SharesTrack(
+            name = request.form["newShare"],
+            dateFrom = request.form["fromDate"]
+        )
+
+        currentUserMd = Users.query.filter_by(email = session['userEmail']).first()
+        currentUserMd.shares.append(newShare)
+        db.session.commit()
+    else:
+        return render_template("register_shares.html", erro = True, currentUser = session['userEmail'])
+
+    return redirect(url_for('registerShares'))
+
+print("fim")
 
 
